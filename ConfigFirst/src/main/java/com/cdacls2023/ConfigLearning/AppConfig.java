77f795a4db11e17459e5a1412b873ad354cba1a8
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-07-16 11:33:44 +0530

    Java Configruation | Part-2 | Custom object as bean

--- ConfigFirst/src/main/java/com/cdacls2023/ConfigLearning/AppConfig.java
+++ ConfigFirst/src/main/java/com/cdacls2023/ConfigLearning/AppConfig.java
@@ -3,6 +3,7 @@ package com.cdacls2023.ConfigLearning;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Address(String addressline, String city) {}
 @Configuration
 public class AppConfig {
 	
@@ -14,5 +15,15 @@ public class AppConfig {
 	@Bean
 	public int age() {
 		return 30;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Rahul", 25);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("V.T. Road, Mansarovar", "Jaipur");
 	}
 }
---
+++ ConfigFirst/src/main/java/com/cdacls2023/ConfigLearning/Person.java
@@ -0,0 +1,25 @@
+package com.cdacls2023.ConfigLearning;
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+}
