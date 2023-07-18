771b61cfc6861d0f90d5f647cafaa044ac28c650
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-07-18 09:33:12 +0530

    Construction injection

--- Sixth/src/main/java/com/cdacls2023/DependencyInjection/MainApp.java
+++ Sixth/src/main/java/com/cdacls2023/DependencyInjection/MainApp.java
@@ -11,19 +11,27 @@ class BusinessLogic {
 	private Dependency1 dependency1;
 	private Dependency2 dependency2;
 	
-	@Override
-	public String toString() {
-		return "BusinessLogic [dependency1=" + dependency1 + ", dependency2=" + dependency2 + "]";
-	}
 	
-	@Autowired
+	@Autowired // Optional - By default
+	public BusinessLogic(Dependency1 dependency1, Dependency2 dependency2) {
+		super();
+		this.dependency1 = dependency1;
+		this.dependency2 = dependency2;
+	}
+
+
 	public void setDependency1(Dependency1 dependency1) {
 		this.dependency1 = dependency1;
 	}
 
-	@Autowired
+	
 	public void setDependency2(Dependency2 dependency2) {
 		this.dependency2 = dependency2;
+	}
+	
+	@Override
+	public String toString() {
+		return "BusinessLogic [dependency1=" + dependency1 + ", dependency2=" + dependency2 + "]";
 	}
 }
 
