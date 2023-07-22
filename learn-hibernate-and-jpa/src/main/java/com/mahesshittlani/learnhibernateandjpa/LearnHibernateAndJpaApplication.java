d778eeab381bce3377b57f717a7e20cd0172017a
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-07-22 10:56:12 +0530

    Spring jdbc | Connecting with Database using jdbc driver

---
+++ learn-hibernate-and-jpa/src/main/java/com/mahesshittlani/learnhibernateandjpa/LearnHibernateAndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mahesshittlani.learnhibernateandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHibernateAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHibernateAndJpaApplication.class, args);
+	}
+
+}
---
+++ learn-hibernate-and-jpa/src/test/java/com/mahesshittlani/learnhibernateandjpa/LearnHibernateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesshittlani.learnhibernateandjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHibernateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
